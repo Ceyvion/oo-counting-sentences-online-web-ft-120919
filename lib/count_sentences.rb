b522a909fb525fc7f792c9a0f9c176cf81bee462
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-19 22:20:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,19 @@ class String
   end
 
   def count_sentences
-   self.split(" . " " ? ").count
+  split_characters = [".","!","?"]
+  new_arr = []
+    string_arr = self.split(/[.?!]+/)
+    string_arr.select do |word|
+      if word.length != 0
+        new_arr << word
+      elsif
+        string_arr.size == 0
+        return 0
+      else 
+        return string_arr.size
+      end
+    end
+    return new_arr.size
   end
 end
